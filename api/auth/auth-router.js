87657a438d3ed6e34f9b3ada1f368a7efa69ee77
Zachary Honey <zhoney466@gmail.com>
2023-11-04 09:29:30 -0700

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,16 +1,25 @@
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const router = express.Router()
+const User = require('../users/users-model')
 
 
 router.post('/register', async (req, res, next) => {
-    res.json({ message: 'register working'})
+    try {
+    const { username, password } = req.body
+    const hash = bcrypt.hashSync(password, 8)
+    const newUser = { username, password: hash }
+    const result = await User.add(newUser)
+    res.status(201).json(result)
+    } catch(err) {
+        next({ message: 'something went wrong inside the register router'})
+    }
 })
 router.post('/login', async (req, res, next) => {
-    res.json({ message: 'login working'})
+    res.json({ message: 'login working' })
 })
 router.get('/logout', async (req, res, next) => {
-    res.json({ message: 'logout working'})
+    res.json({ message: 'logout working' })
 })
 
 module.exports = router;
